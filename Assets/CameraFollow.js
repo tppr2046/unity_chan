71da11f50072f1b430e96148f3c1c02c76cba057
jim <gopmaster@hotmail.com>
2014-08-01 11:29:55 +0800

    unitychantest
    for test

---
+++ Assets/CameraFollow.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var target: Transform;
+var distanceX: float;
+var distanceY: float;
+var distanceZ: float;
+
+function Start () {
+
+}
+
+function Update () {
+	
+	this.transform.position.x = target.transform.position.x + distanceX;
+	this.transform.position.y = target.transform.position.y + distanceY;
+	this.transform.position.z = target.transform.position.z + distanceZ;
+
+
+
+}
